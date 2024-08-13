e4d5bd8d79a3f89542ea01a52323f404f8800ee1
leoda <2268185728@qq.com>
2024-08-13 20:12:12 +0800

    code

--- code/test_warp/inc/timer.hpp
+++ code/test_warp/inc/timer.hpp
@@ -11,8 +11,8 @@ public:
     using ns = std::ratio<1, 1000000000>;
 
 public:
-    Timer();
-    ~Timer();
+    Timer();    // 构造函数
+    ~Timer();   // 析构函数
 
 public:
     void start_cpu();
@@ -21,14 +21,21 @@ public:
     void stop_gpu();
 
     template<typename span>
-    void duration_cpu(std::string msg);
-    void duration_gpu(std::string msg);
+    void duration_cpu(std::string msg); // 用于在CPU上测量时间间隔的函数，接受一个字符串消息作为参数
+    void duration_gpu(std::string msg); // 用于在GPU上测量时间间隔的函数，接受一个字符串消息作为参数
 
+// 定义一个计时器类，用于记录和计算时间
+class Timer {
 private:
+    // 记录CPU开始时间的时间点
     std::chrono::time_point<std::chrono::high_resolution_clock> _cStart;
+    // 记录CPU结束时间的时间点
     std::chrono::time_point<std::chrono::high_resolution_clock> _cStop;
+    // CUDA事件，用于记录GPU开始时间
     cudaEvent_t _gStart;
+    // CUDA事件，用于记录GPU结束时间
     cudaEvent_t _gStop;
+    // 记录的时间间隔，单位为秒
     float _timeElasped;
 };
 
