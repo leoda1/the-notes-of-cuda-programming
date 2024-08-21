62506cbd1c2ba5738990c7ee4995b90a77a7671f
leoda <2268185728@qq.com>
2024-08-21 21:41:57 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -1,3 +1,4 @@
+/*nixudui.cpp
 #include <iostream>
 
 using namespace std;
@@ -9,7 +10,7 @@ LL nixudui(int l, int r)
 {
     if (l >= r) return 0;
     int mid = (l + r) >> 1;
-    LL res = nixudui(l, mid) + nixudui(mid+1, r);
+    LL res = nixudui(l, mid) + nixudui(mid + 1, r);
     //merge sort开始
     int k = 0, i = l, j = mid + 1;
     while ( i <= mid && j <= r)
@@ -21,7 +22,7 @@ LL nixudui(int l, int r)
         }
     while (i <= mid) t[k++] = q[i++];
     while (j <= r) t[k++] = q[j++];
-    for (int i = l; i <= r; i++) q[i] = t[i-l];
+    for (int i = l, j = 0; i <= r; i++, j++) q[i] = t[j];
     //merge sort结束
     return res;
 }
@@ -32,5 +33,39 @@ int main()
     for (int i = 0; i < n; i++) cin >> q[i];
     cout << nixudui(0, n-1) << endl;
     for (int i = 0; i < n; i++) cout << q[i];
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+const int N = 100010;
+int n, m;
+int q[N];
+
+int main(){
+    scanf("%d%d", &n, &m);
+    for ( int i = 0; i < n; i++ ) cin >> q[i];
+    while ( m-- ) {
+        int x;
+        cin >> x;
+
+        int l = 0, r = n-1;
+        while ( l < r ){
+            int mid = l + r >> 1;
+            if ( q[mid] >= x ) r = mid;
+            else l = mid + 1;
+        }
+        if (q[l] != x) cout << "-1 -1" << endl;
+        else{
+            cout << l << ' ';
+            int l = 0, r = n - 1;
+            while ( l < r){
+                int mid = l + r + 1 >> 1;
+                if ( q[mid] <= x ) l = mid;
+                else r = mid - 1; 
+            }
+            cout << l << endl;
+        }
+    }
     return 0;
 }
