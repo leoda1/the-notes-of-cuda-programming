b02d21012b1f4e9548e57784e0a9cd9f0c2d5e76
leoda <2268185728@qq.com>
2024-08-24 16:30:50 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -155,3 +155,40 @@ int main(){
     return 0;
 
 }*/
+/*高精度乘以低精度
+#include <iostream>
+#include <vector>
+const int N = 1e6 + 10;
+
+using namespace std;
+
+vector<int> matmul(vector<int> &A, int b)
+{
+    vector<int> C;
+    int t = 0;
+    for (int i = 0; i < A.size(); i++){
+        t += A[i] * b;
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    return C;
+
+}
+
+
+int main()
+{
+    string a;
+    int b;
+    cin >> a >> b;
+    vector<int> A;
+    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0');
+
+    auto C = matmul(A, b);
+
+    for (int i = C.size() - 1; i >= 0; i--) cout << C[i];
+    cout << endl;
+    return 0;
+
+
+}*/
