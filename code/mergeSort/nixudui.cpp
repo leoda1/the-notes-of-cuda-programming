dc9e9e35f0be789a76a5f3a2c669c063a2862bdc
leoda <2268185728@qq.com>
2024-08-25 12:02:33 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -192,3 +192,42 @@ int main()
 
 
 }*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const int N = 1e6 + 10;
+// C = A / B
+vector<int> div(vector<int> &A, int b, int &r)
+{
+    vector<int> C;
+    r = 0;
+    for (int i = A.size() - 1; i >= 0; i--){
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r %= b;
+    }
+    reverse(C.begin(), C.end()); // 将C倒序输出
+    while (C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+
+
+int main()
+{
+    string a;
+    int b;
+    cin >> a >> b;
+
+    vector<int> A;
+    for (int i = a.size() - 1; i >= 0; i-- ) A.push_back(a[i] - '0');
+
+    int r;
+    auto C = div(A, b, r);
+
+    for (int i = C.size() - 1; i >= 0; i--) cout << C[i];
+    cout << endl;
+    cout << r << endl;
+    return 0;
+}
