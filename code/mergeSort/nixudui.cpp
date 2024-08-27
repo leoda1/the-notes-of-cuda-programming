c8a2900d379de02e20505554d70943f2365b7821
leoda <2268185728@qq.com>
2024-08-27 21:25:35 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -155,3 +155,27 @@ int main(){
     return 0;
 
 }*/
+
+
+
+#include <iostream>
+using namespace std;
+const int N = 100000;
+int sum[N] = {0};
+
+int main()
+{
+    int n, m, x;
+    int a[N];
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++){
+        cin >> x;
+        sum[i] = x + sum[i - 1];
+    }
+    while(m--){
+        int l, r;
+        cin >> l >> r;
+        cout << sum[r] - sum[l - 1] << endl;
+    }
+    return 0;
+}
