a52b1f6c4450ff4822aebdd0a7b7017e8c2fa605
leoda <2268185728@qq.com>
2024-09-08 17:52:46 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -253,3 +253,31 @@ int main()
     }
     return 0;
 }*/
+
+
+/*子矩阵 sub of matrix*/
+#include <iostream>
+using namespace std;
+const int N = 10010;
+int sum[N][N] = {{0,0}};
+
+int main()
+{
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            int  x = 0;
+            cin >> x;
+            sum[i][j] = x + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+        }       
+    }
+    while(q--)
+    {
+        int x1,y1,x2,y2;
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+        printf("%d\n", sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][y1 - 1]);
+    }
+    return 0;
+}
