968be9f0c64ed5c794ac1836c22ec96a70d3ce55
leoda <2268185728@qq.com>
2024-09-19 23:32:09 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -316,3 +316,45 @@ int main()
         cout << b[i] << " ";
     return 0;
 }*/
+
+/*差分矩阵*/
+#include<iostream>
+const int N = 10010;
+using namespace std;
+int a[N][N] = {0}, b[N][N]= {0}, q[N] = {0};
+void insert(int x1, int y1, int x2, int y2, int c)
+{
+    b[x1][y1] += c;
+    b[x1][y2 + 1] -= c;
+    b[x2 + 1][y1] -= c;
+    b[x2 + 1][y2 + 1] += c;
+
+}
+int main()
+{
+    int n, m ,q;
+    cin >> n >> m >> q;
+    for (int i = 1; i<= n; i++){
+        for (int j = 1; j<= m; j++){
+            cin >> a[i][j];
+            insert(i, j, i, j, a[i][j]);
+        }
+    }
+    while(q--){
+        int x1, y1, x2, y2, c;
+        cin >> x1 >> y1 >> x2 >> y2 >> c;
+        insert(x1, y1, x2, y2, c);
+    }
+    for (int i = 1; i<= n; i++){
+        for (int j = 1; j<= m; j++){
+            b[i][j] += b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1];
+            printf("%d ", b[i][j]);
+        }
+    }
+    for (int i = 1; i<= n; i++){
+        for (int j = 1; j<= m; j++){
+           cout >> b[i][j];
+        }
+    }
+    return 0;
+}
