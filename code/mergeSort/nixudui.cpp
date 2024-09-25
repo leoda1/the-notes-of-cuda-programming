57b69139ba4fa03374ad94d3fd64f8d579cafdbe
leoda <2268185728@qq.com>
2024-09-25 14:42:06 +0800

    code

--- code/mergeSort/nixudui.cpp
+++ code/mergeSort/nixudui.cpp
@@ -358,3 +358,28 @@ int main()
     }
     return 0;
 }*/
+
+/*最长连续不重复子序列*/
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+const int N = 100010;
+int a[N], s[N];
+
+int main()
+{
+    int n, res = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0, j = 0; i < n; i++){
+        s[a[i]] ++;
+        while ( s[a[i]] > 1 )
+        {
+            s[a[j++]] --;
+        }
+        res = max(res, i - j + 1);
+    }
+    cout << res << endl;
+    return 0;
+}
