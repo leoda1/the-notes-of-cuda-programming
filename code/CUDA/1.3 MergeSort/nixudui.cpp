231835fce01fb76a2358157576cf538c759f42c7
leoda <2268185728@qq.com>
2024-10-10 10:44:47 +0800

    code

--- code/CUDA/1.3 MergeSort/nixudui.cpp
+++ code/CUDA/1.3 MergeSort/nixudui.cpp
@@ -508,3 +508,59 @@ int main()
     }
     return 0;
 }*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const int N = 300010;
+int a[N], s[N];
+vector<int> alls;
+vector<pair<int, int>> add, query;
+
+int find(int x) { //返回的是输入的坐标的离散化下标
+    int l = 0, r = alls.size() - 1;
+    while (l < r) {
+        int mid = l + r >> 1;
+        if (alls[mid] >= x) r = mid;
+        else l = mid + 1;
+    }
+    return r + 1;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for ( int i = 1; i <= n; i++){
+        int x, c;
+        cin >> x >> c;
+        add.push_back({x, c});
+        alls.push_back(x);
+    }
+    for ( int i = 1; i <= m; i++){
+        int l, r;
+        cin >> l >> r;
+        query.push_back({l, r});
+        alls.push_back(l);
+        alls.push_back(r);
+    }
+    sort(alls.begin(), alls.end());
+    alls.erase(unique(alls.begin(), alls.end()), alls.end());
+
+    for (auto item : add) {
+        int x = find(item.first);
+        a[x] += item.second;
+    }
+    //前缀和
+    for (int i = 1; i <= alls.size(); i++) s[i] = s[i-1] + a[i];
+    //处理后m次询问操作
+    for (auto item : query) {
+        int l = find(item.first);
+        int r = find(item.second);
+        printf("%d\n", s[r] - s[l-1]);
+    }
+
+    return 0;
+}
