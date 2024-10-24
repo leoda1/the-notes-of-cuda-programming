7762b4125fbe46119d2be8d8245c5b7327f9435e
leoda <2268185728@qq.com>
2024-10-24 21:17:24 +0800

    add notes

--- code/TensorRT/2.8 Load_model/src/cpp/model.cpp
+++ code/TensorRT/2.8 Load_model/src/cpp/model.cpp
@@ -15,17 +15,16 @@ using namespace std;
 
 class Logger : public nvinfer1::ILogger{
 public:
-    using Severity = nvinfer1::ILogger::Severity;
 
-    void log (Severity severity, const char* msg) noexcept override{
+    // using Severity = nvinfer1::ILogger::Severity;
+    virtual void log (Severity severity, const char* msg) noexcept override{
         string str;
         switch (severity){
-            case Severity::kINTERNAL_ERROR: str = RED    "[fatal]:" CLEAR; break;
-            case Severity::kERROR:          str = RED    "[error]:" CLEAR; break;
-            case Severity::kWARNING:        str = BLUE   "[warn]:"  CLEAR; break;
-            case Severity::kINFO:           str = YELLOW "[info]:"  CLEAR; break;
-            case Severity::kVERBOSE:        str = PURPLE "[verb]:"  CLEAR; break;
-            default:                        str = "[default]:"; break;
+            case Severity::kINTERNAL_ERROR: str = RED    "[fatal]:" CLEAR;
+            case Severity::kERROR:          str = RED    "[error]:" CLEAR;
+            case Severity::kWARNING:        str = BLUE   "[warn]:"  CLEAR;
+            case Severity::kINFO:           str = YELLOW "[info]:"  CLEAR;
+            case Severity::kVERBOSE:        str = PURPLE "[verb]:"  CLEAR;
         }
         if (severity <= Severity::kINFO)
             cout << str << string(msg) << endl;
@@ -68,19 +67,22 @@ bool Model::build(){
     auto config        = unique_ptr_with_deleter<nvinfer1::IBuilderConfig>(builder->createBuilderConfig());
     auto parser        = unique_ptr_with_deleter<nvonnxparser::IParser>(nvonnxparser::createParser(*network, logger));
 
+    // 设置工作区内存池的最大内存为 256 MB
     config->setMemoryPoolLimit(nvinfer1::MemoryPoolType::kWORKSPACE, 1 << 28);
-
+    
+    // 用parser解析onnx文件
     if (!parser->parseFromFile(mOnnxPath.c_str(), 1)){
         LOGE("ERROR: failed to %s", mOnnxPath.c_str());
         return false;
     }
 
-    // auto engine        = unique_ptr_with_deleter<nvinfer1::ICudaEngine>(builder->buildEngineWithConfig(*network, *config));
+    //auto engine        = unique_ptr_with_deleter<nvinfer1::ICudaEngine>(builder->buildEngineWithConfig(*network, *config));
     // if (!engine){
     //         LOGE("Failed to build engine.");
     //         return false;
     // }
 
+    // 序列化engine
     auto plan          = builder->buildSerializedNetwork(*network, *config);
     if (!plan) {
         LOGE("Failed to build serialized network.");
@@ -93,11 +95,14 @@ bool Model::build(){
         return false;
     }
 
+    //
     auto f = fopen(mEnginePath.c_str(), "wb");
     fwrite(plan->data(), 1, plan->size(), f);
     fclose(f);
-
+    // 通过IRuntime的反序列化api生成反序列化引擎
     mEngine            = shared_ptr<nvinfer1::ICudaEngine>(runtime->deserializeCudaEngine(plan->data(), plan->size()), InferDeleter());
+    
+    // 通过 network-> 就可以看自动补全 有那些可以打印 打印的API开头是get
     mInputDims         = network->getInput(0)->getDimensions();
     mOutputDims        = network->getOutput(0)->getDimensions();
     LOG("Input dim is %s", printDims(mInputDims).c_str());
