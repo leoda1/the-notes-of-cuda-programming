5b4aaf87725a60b3c4f20b063bd8bad5b267525d
leoda <2268185728@qq.com>
2025-01-08 22:54:24 +0800

    update reduce_v1 & v2

---
+++ code/CUDA/1.1 Reduce/reduce_v1_interleaved_address.cu
@@ -0,0 +1,61 @@
+#include <cuda.h>
+#include <iostream>
+
+#define N 32*1024*1024
+#define BLOCK_SIZE 256
+
+__global__ void reduce_baseline (float * g_idata, float * g_odata) {
+    __shared__ float sdata[BLOCK_SIZE];
+
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockDim.x * blockIdx.x + tid;
+    sdata[tid] = g_idata[idx];
+    __syncthreads();
+
+    // do reduction in shared memory
+    for (unsigned int s = 1; s < blockDim.x; s *= 2) {
+        int index = 2 * s * tid;
+        if (index < blockDim.x) {
+            sdata[index] += sdata[index + s];
+        }
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+int main() {
+    float *input_host = (float *)malloc(N * sizeof(float));
+    float *input_device;
+    cudaMalloc((void **)&input_device, N * sizeof(float));
+    for (int i = 0; i < N; i++) input_host[i] = 2.0;
+    cudaMemcpy(input_device, input_host, N*sizeof(float), cudaMemcpyHostToDevice);
+
+    int32_t block_num = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    float *output_host = (float*)malloc((N / BLOCK_SIZE) * sizeof(float));
+    float *output_device;
+    cudaMalloc((void **)&output_device, (N / BLOCK_SIZE) * sizeof(float));
+    
+    dim3 grid(N / BLOCK_SIZE, 1);
+    dim3 block(BLOCK_SIZE, 1);
+    reduce_baseline<<<grid, block>>>(input_device, output_device);
+    
+    cudaDeviceSynchronize();
+    cudaMemcpy(output_host, output_device, block_num * sizeof(float), cudaMemcpyDeviceToHost);
+    
+    float final = 0.0;
+    for (int i = 0; i < 10; i++) {
+    std::cout << "Block " << i << " result: " << output_host[i] << std::endl;
+        final += output_host[i];  // Sum up the block results
+    }
+    std::cout << "Final result after reduction: " << final << std::endl;
+
+    // Free memory
+    cudaFree(input_device);
+    cudaFree(output_device);
+    free(input_host);
+    free(output_host);
+    return 0;
+}
