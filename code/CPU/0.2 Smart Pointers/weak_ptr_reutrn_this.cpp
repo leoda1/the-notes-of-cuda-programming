02eefd0dfa452e55a58b561cec02eab06ec61bb1
leoda <2268185728@qq.com>
2025-03-04 11:54:36 +0800

    Initial commit

--- code/CPU/0.2 Smart Pointers/weak_ptr_reutrn_this.cpp
+++ code/CPU/0.2 Smart Pointers/weak_ptr_reutrn_this.cpp
@@ -21,13 +21,14 @@ int main()
     // auto spp = make_shared<A>();
     shared_ptr<A> sp1(new A);
     shared_ptr<A> sp2 = sp1->GetSelf();  // ok
-    //    shared_ptr<A> sp2;
-    //    {
-    //        shared_ptr<A> sp1(new A);
-    //        sp2 = sp1->GetSelf();  // ok
-    //    }
+    // shared_ptr<A> sp2;
+    // {
+    //     shared_ptr<A> sp1(new A);
+    //     sp2 = sp1->GetSelf();  // ok
+    // }
     cout << "sp1.use_count() = " << sp1.use_count()<< endl;
     cout << "sp2.use_count() = " << sp2.use_count()<< endl;
+
 
     return 0;
 }
